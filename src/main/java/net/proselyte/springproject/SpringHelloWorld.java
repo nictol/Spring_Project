8d962225295a8cf2c33f604810c73f3ed7a48cfd
Tolkachev Nikita <nictol76@yandex.ru>
2020-02-25 18:19:40 +0300

    Initial commit

---
+++ src/main/java/net/proselyte/springproject/SpringHelloWorld.java
@@ -0,0 +1,13 @@
+package net.proselyte.springproject;
+
+public class SpringHelloWorld {
+    private String helloMessage;
+
+    public void setHelloMessage(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
+    public void getHelloMessage() {
+        System.out.println("Hello message: " + helloMessage);
+    }
+}
